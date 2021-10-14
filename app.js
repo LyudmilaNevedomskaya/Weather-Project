bab222d6c53c5ceb67b27c84fae4c627e6df6b63
Lyudmila <milo4kamoya@gmail.com>
2021-10-14 00:36:04 -0700

    Input field added, data for current city displayed

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 const port = 3001;
 
 app.get("/", (req, res) => {
   //res.send("Server is running");
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=acb26e7d0729d6f9d84f43af38a21e4d";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  //console.log(req.body.cityName);
+  const cityQuery = req.body.cityName;
+  const apiKey = "acb26e7d0729d6f9d84f43af38a21e4d";
+  const unit = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityQuery}&units=${unit}&appid=${apiKey}`;
   https.get(url, (response) => {
     // console.log("response", response);
     // console.log("statusCode:", response.statusCode);
@@ -20,7 +29,7 @@ app.get("/", (req, res) => {
       const icon = weatherData.weather[0].icon;
       const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
       res.write(
-        `<h1>The temperature in Vancouver is ${temp} degrees Celcius</h1>`
+        `<h1>The temperature in ${cityQuery} is ${temp} degrees Celcius</h1>`
       );
       res.write(`<p>The weather is currently ${description}</p>`);
       res.write(`<img src="${imageUrl}" alt="weather icon">`);
